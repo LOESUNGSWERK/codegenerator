49aae1bb510899806d0bd92ba9a63d7cf3a480f6
Rene Kuehle <r.kuehle@traso.de>
2017-06-01 14:43:32 +0200

    Tree-Funktionalität eingebaut

--- create.php
+++ create.php
@@ -10,3 +10,5 @@ $loader = include __DIR__.'/vendor/autoload.php';
 error_reporting(E_ALL & ~E_NOTICE );
 $creator = new \RkuCreator\Creator();
 $creator->run();
+
+exec('cp -rvf  /home/rkuehle/project/www_root/codegenerator/projects/xresSettings/dist/zendFramework/module/SettingsUi /home/rkuehle/project/tutorial/module/');
